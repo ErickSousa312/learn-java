c49770616d59f6ea655a9b142714ff9e06de5662
ErickSousa312 <58636303+ErickSousa312@users.noreply.github.com>
2024-06-01 15:10:50 -0300

    create system file reader and writer

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
 
-import entities.Account;
 import entities.BusinessAccount;
 
-import java.util.Date;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        BusinessAccount businessAccount = new BusinessAccount();
+        String [] lines = new String[] {"good morning", "good afternoon", "good night"};
+
+        String path= "C:\\Users\\erik-pc\\IdeaProjects\\learn-java\\src\\out.txt";
+        
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        }catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
